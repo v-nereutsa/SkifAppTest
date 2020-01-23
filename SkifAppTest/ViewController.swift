d010f8b3537359e952663809d11aca942509be3e
v-nereutsa <45881935+v-nereutsa@users.noreply.github.com>
2020-01-23 13:54:33 +0300

    init commit

---
+++ SkifAppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkifAppTest
+//
+//  Created by Владимир Нереуца on 23.01.2020.
+//  Copyright © 2020 Владимир Нереуца. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
